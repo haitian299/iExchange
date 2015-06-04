2de3bdbc11ff2c931fdb6c67233045272546a9f0
金海天 <haitian299@gmail.com>
2015-06-04 23:55:20 +0800

    Initial Commit

---
+++ iExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iExchange
+//
+//  Created by JIN on 15/6/4.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iExchange
+//
+//  Created by JIN on 15/6/4.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
